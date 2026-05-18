897fc017cdbe006940101500bd930e07fb54b87b
Ke-no <S10275096@connect.np.edu.sg>
2026-05-19 02:03:48 +0800

    Update code

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -3,18 +3,4 @@ using UnityEngine;
 public class Collectibles : MonoBehaviour
 {
     public int score = 1;
-    void OnCollisionEnter(Collision collision)
-    {
-        print("Player collided with " + collision.gameObject.name);
-    }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 }
